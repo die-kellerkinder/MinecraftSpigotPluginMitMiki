7bdc11c38668452fc08d2ff105dfba11de1dce87
Mara Graf <qt3.14batman@gmail.com>
2019-07-13 22:45:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple-Minecraft-Command
+ 
---
+++ src/my/first/plugin/Main.java
@@ -0,0 +1,29 @@
+package my.first.plugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Main extends JavaPlugin {
+	// Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender,
+            Command command,
+            String label,
+            String[] args) {
+        if (command.getName().equalsIgnoreCase("mycommand")) {
+            sender.sendMessage("You ran /mycommand!");
+            return true;
+        }
+        return false;
+    }
+}
